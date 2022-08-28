6f6833816322434fc93928eb5ecf505d8462053d
Richard Hanrahan <fahitch@gmail.com>
2022-08-29 00:17:16 +0100

    Adds content import from Dato CMS on command

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ The site is built using the Jekyll engine in order to utilise the very generous 
 In order to understand the files, you must have knowledge of [Markdown](https://www.markdownguide.org/) and [Liquid](https://shopify.github.io/liquid/), which together provide the templating languages the site uses.
 
 Due to the nature of these tools, the code involved is lightweight, and uses CSS to provide the more dynamic display elements.
+
+
+The content is provided by an external Dato CMS backend. In order to run this, use the CLI command `dato dump`
+
+In order for this to work, you need to define an .env for the api token, using 'DATO_API_TOKEN' as the key. Otherwise, this can be provided in the command.
+`dato dump --token=abc123`
+
+This should populate the _posts folder, as well as the subpages _data folder.
+
+When you are ready to run the site, run
+`bundle exec jekyll serve`
---
+++ dato.config.rb
@@ -0,0 +1,59 @@
+# https://www.rubydoc.info/gems/dato/0.8.2/Dato/Dump/Dsl
+
+subpages = dato.dev_portfolio_sub_pages.map do | subpage |
+
+  {
+    id: subpage.ref,
+    title: subpage.title,
+    order: subpage.try(:order),
+    summary: subpage.summary,
+    work_history: subpage.work_history.map do | entry |
+      {
+        title: "#{entry.role} (#{entry.date_started} - #{entry.date_ended or "present"})"
+      }
+    end
+  }
+end
+
+# Sorts content by page order defined by CMS
+subpages.sort_by! do |page|
+  page[:order]
+end
+
+create_data_file(
+  "_data/subpages.yml",
+  :yaml,
+  subpages)
+
+
+directory "_posts" do
+
+  dato.articles.each do |item|
+
+# filter out content not intended for this site
+    if item.try(:display_on_dev_portfolio) == true
+      create_post "#{item.updated_at.to_s[0,10]}-#{item.title.parameterize}.md" do
+
+        frontmatter :yaml, {
+          layout: item.layout.name,
+          title: item.title,
+          date: item.updated_at.to_s,
+          categories: item.category.title,
+          banner: item.banner_image.try(:url),
+          thumbnail: item.thumbnail.try(:url),
+          banner_caption: item.banner_image.try(:url),
+          image: item.banner_image.try(:url),
+          company: "Freelance",
+          summary: item.summary,
+          tags: item.try(:tags)
+        }
+
+        content item.markdown
+
+      end
+    else
+      break
+    end
+  end
+
+end
